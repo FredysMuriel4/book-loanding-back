dd42c32ce6c39ec980a6a4928d8e75c15dbc1883
Fredysmuriel4 <fredysmuriel@gmail.com>
2025-04-07 00:56:00 -0500

    Route to get student made

--- src/main/java/com/BookLoanding/book_loanding/student/api/StudentController.java
+++ src/main/java/com/BookLoanding/book_loanding/student/api/StudentController.java
@@ -1,6 +1,8 @@
 package com.BookLoanding.book_loanding.student.api;
 
+import com.BookLoanding.book_loanding.category.model.Category;
 import com.BookLoanding.book_loanding.student.dto.StudentDTO;
+import com.BookLoanding.book_loanding.student.model.Student;
 import com.BookLoanding.book_loanding.student.request.StudentRequest;
 import com.BookLoanding.book_loanding.student.service.StudentService;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,12 @@ public class StudentController {
     @PostMapping("/save")
     public StudentDTO createStudent(@Validated @RequestBody StudentRequest studentRequest) {
         return studentService.storeStudent(studentRequest);
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable Short id) {
+
+        return studentService.getStudentById(id);
     }
 
     @PutMapping("/update/{id}")
